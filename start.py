b6551381b9e23c539a75fd789610dca7fce5baa4
Nat1an <nathan@nat1an.xyz>
2023-03-25 19:06:35 +0800

    Reformat start.py

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
-import discord,json
+import discord
+import json
 
 intents = discord.Intents.default()
 intents.members = True
@@ -9,9 +10,9 @@ bot = discord.Bot(
 )
 
 if __name__ == "__main__":
-    #載入Cogs
+    # 載入Cogs
     for key, value in bot.load_extension("cogs", recursive=True, store=True).items():
-        if value == True:
+        if value:
             print(f"成功載入插件 {key}")
         else:
             print(f"載入插件 {key} 失敗: {value}")
